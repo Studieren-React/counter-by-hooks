46f6cddd8053c7ab38e8d7fea2ff642478d084e5
Verda Muso <gearmobile@gmail.com>
2024-04-30 18:35:51 +0300

    useEffects

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
+import { useState } from 'react'
 import './App.css'
 import { Clicker } from './components/Clicker'
 
 export default function App() {
+  const [busy, handleBusy] = useState(false);
+
   return (
-    <Clicker />
+    <div className="App">
+      <h3>React Life Cycle && Hooks</h3>
+      <button onClick={() => handleBusy(!busy)}>toggle clicker</button>
+      {busy && <Clicker />}
+    </div>
   )
 }
